d36ba3bd0dfd3c5e966db8de464f3d2fa6a2163f
Kanda Jean Joel <90659970+Jean-joel@users.noreply.github.com>
2025-05-19 11:15:15 +0000

    Add files via upload

--- script.js
+++ script.js
@@ -43,6 +43,82 @@ window.onclick = function(e) {
   }
 };
 
+const images = [
+  "assets/images/rendus/bot22.png",
+  "assets/images/rendus/btei.png",
+  "assets/images/rendus/cadeau.png",
+  "assets/images/rendus/cadeaun.png",
+  "assets/images/rendus/carrefour.png",
+  "assets/images/rendus/croix.png",
+  "assets/images/rendus/loupe.png",
+  "assets/images/rendus/tab_chaise.png",
+  "assets/images/rendus/terrasse.png",
+  "assets/images/rendus/ven_vin.png",
+  "assets/images/rendus/verre.png",
+  "assets/images/rendus/vin lunette.png",  
+  "assets/images/rendus/vin_unette_venti.png",
+];
+let currentImageIndex = 0;
+
+function openCarouselModal() {
+  currentImageIndex = 0;
+  document.getElementById("carouselImage").src = images[currentImageIndex];
+  document.getElementById("carouselModal").style.display = "block";
+}
+
+function closeCarouselModal() {
+  document.getElementById("proj6-r").style.display = "none";
+}
+
+function nextSlide() {
+  currentImageIndex = (currentImageIndex + 1) % images.length;
+  document.getElementById("carouselImage").src = images[currentImageIndex];
+}
+
+function prevSlide() {
+  currentImageIndex = (currentImageIndex - 1 + images.length) % images.length;
+  document.getElementById("carouselImage").src = images[currentImageIndex];
+}
+
+// 🔁 Carrousel vidéo
+const videos = [
+  "assets/animation/aniversaire_y.mkv",
+  "assets/animation/feux_artifice.mkv",
+  "assets/animation/si.mkv"
+];
+let currentVideoIndex = 0;
+
+function openVideoCarouselModal() {
+  currentVideoIndex = 0;
+  loadVideo(currentVideoIndex);
+  document.getElementById("videoCarouselModal").style.display = "block";
+}
+
+function closeVideoCarouselModal() {
+  const videoEl = document.getElementById("carouselVideo");
+  videoEl.pause();
+  document.getElementById("proj6-v").style.display = "none";
+}
+
+function loadVideo(index) {
+  const videoEl = document.getElementById("carouselVideo");
+  videoEl.pause();
+  const sourceEl = videoEl.querySelector("source");
+  sourceEl.src = videos[index];
+  videoEl.load();
+  videoEl.play();
+}
+
+function nextVideo() {
+  currentVideoIndex = (currentVideoIndex + 1) % videos.length;
+  loadVideo(currentVideoIndex);
+}
+
+function prevVideo() {
+  currentVideoIndex = (currentVideoIndex - 1 + videos.length) % videos.length;
+  loadVideo(currentVideoIndex);
+}
+
 // Loader
 window.onload = () => {  
 
